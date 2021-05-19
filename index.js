6930abde7aba3ef27cda13198f3a99fdc2283665
CSH <64762380+CSH3235@users.noreply.github.com>
2021-05-19 11:54:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 d3.selectAll('.span_hor')
     .datum(function(){return this.dataset;})
-    .style("width","10%") // 초기 값
+    .style("width","20%") // 초기 값
     .transition().duration(1500) // 1.5초
     .style("width",d =>d.val); // duration 동안 움직이는 최종 값
+
 
 
 var w = 250, h = 250;
